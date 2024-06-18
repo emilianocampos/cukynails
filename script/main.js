7ceb013a04825e266ee44427b330264696da64b4
emilianocampos <emicampos1268@gmail.com>
2024-06-18 18:47:20 -0300

    asd

--- script/main.js
+++ script/main.js
@@ -11,30 +11,3 @@ burgerMenu.addEventListener('click', function () {
     this.classList.toggle("animated");
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var glider = new Glider(document.querySelector('.glider'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: '.dots',
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        },
-        draggable: true,
-        rewind: true,
-        duration: 1.5
-    });
-
-    // Configuración del carrusel automático
-    function autoScroll() {
-        setInterval(function () {
-            if (glider.slide >= glider.slides.length - 1) {
-                glider.scrollItem(0);
-            } else {
-                glider.scrollItem(glider.slide + 1);
-            }
-        }, 4500); // Cambia de imagen cada 3 segundos
-    }
-
-    autoScroll();
-});
